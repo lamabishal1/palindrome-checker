d6821f1a9f33dba81df32330b84ca3af96bd609a
Bishal Lama <orenlama98@gmail.com>
2024-11-14 11:00:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const quotes = [
-    "Continuance is power.",
-    "Stumbling seven times but standing up eight.",
-    "Don't look back, don't look back, there is no dream in the back.",
-    "Believe in people, but believe in yourself a hundred times more.",
-    "A frog in the well knows nothing of the great ocean.",
-    "If you have no failure in life, you will fail in life.",
-    "Time and tide wait for no man."
-]
+const input = document.getElementById("input")
 
-const usedIndexes = new Set()
+function reverseString(str){
+    return str.split("").reverse().join("")
+}
 
-function generateQuote(){
+function check() {
+    const value = input.value
+    const reverse = reverseString(value)
+    
+    if (value == reverse){
+        alert("PALINDROME")
+    }else{
+        alert("NOT PALINDROME")
+    }
 
+    input.value == ""
 }
