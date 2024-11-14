baa2620c20813385e49e72b404f63764724102ab
Bishal Lama <orenlama98@gmail.com>
2024-11-14 10:47:44 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const quotes = [
+    "Continuance is power.",
+    "Stumbling seven times but standing up eight.",
+    "Don't look back, don't look back, there is no dream in the back.",
+    "Believe in people, but believe in yourself a hundred times more.",
+    "A frog in the well knows nothing of the great ocean.",
+    "If you have no failure in life, you will fail in life.",
+    "Time and tide wait for no man."
+]
+
+const usedIndexes = new Set()
+
+function generateQuote(){
+
+}
